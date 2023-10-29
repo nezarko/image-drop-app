3eaf127fa42a7eafd1d0b1dcef13e51aa23acbea
nizare <nizar.ediesat@surecomp.com>
2023-10-29 14:36:57 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,92 @@
+import React, { useEffect, useRef } from 'react';
+import gsap from 'gsap';
+
+import './App.css';
+import Section from './Section';
+
+function App() {
+  // const numSections = 20; // Generate between 10 to 15 sections
+
+  const sections =  (
+    <Section  />)
+
+  // Add a new section at the bottom with a fixed height of 300px and name it "dropedSection"
+  const dropedSection = (
+    <div
+      className="section"
+      id="receiver-section"
+      style={{ height: '800px',width:"95%", marginBottom:"10px", borderTop: '1px red solid' }}
+    ></div>
+  );
+  // Add a new section at the bottom with a fixed height of 300px and name it "droppedSection"
+  const sectionsContainerRef = useRef(null);
+
+  useEffect(() => {
+    const fallImagesInSections = () => {
+      if (sectionsContainerRef.current) {
+        const sectionElements =
+          sectionsContainerRef.current.querySelectorAll('.section');
+
+        sectionElements.forEach((section, index) => {
+          const images = section.querySelectorAll('img');
+          const rect = section.getBoundingClientRect();
+
+          if (
+            rect.top >= 0 &&
+            rect.top < rect.height &&
+            section.id !== 'receiver-section'
+          ) {
+            fallFlowers(section);
+          } else {
+            // section.style.background = "red";
+          }
+        });
+      }
+    };
+
+    window.addEventListener('scroll', fallImagesInSections);
+  }, []);
+
+  function fallFlowers(section) {
+    const flowers = section.querySelectorAll('img');
+    const receiverSection = document.getElementById('receiver-section');
+  
+    const tl = gsap.timeline(1);
+  
+    flowers.forEach((flower, index) => {
+      tl.to(flower, {
+        // x: () => getRandomInt(130, window.innerWidth - 100),
+        y: () => getRandomInt(0, 800),
+        rotation: getRandomRotation(90), // Add rotation animation
+        opacity: 10,
+        duration: 1,
+        ease: 'ease-in-out',
+      });
+  
+      tl.add(() => {
+        receiverSection.appendChild(flower);
+      }, `-=${0.1}`);
+    });
+  }
+  
+  function getRandomRotation() {
+    // Generate a random rotation value between -180 and 180 degrees
+    return getRandomInt(-180, 180);
+  }
+  
+
+  function getRandomInt(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+
+  return (
+    <div className="App">
+      <div className="sections" ref={sectionsContainerRef}>
+        {sections}
+        {dropedSection}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Section.js
@@ -0,0 +1,89 @@
+import React, { useState, useEffect, memo } from "react";
+import "./App.css";
+
+const getImageUrl = () => {
+  const imageIndex = Math.floor(Math.random() * 3) + 2;
+  return `/imgs/Boys-0${imageIndex}.png`;
+};
+
+const Image = memo(({ sectionHeight }) => {
+  const imageUrl = getImageUrl();
+  const imageStyle = {
+    top: `${Math.random() * (sectionHeight - 100)}px`,
+    left: `${Math.random() * 95}%`,
+    position: "absolute",
+  };
+
+  const tooltipText = "Name: Ahmad , Age : 3 Years, Gender: Male "; // Replace with your tooltip data
+
+  return (
+    <img
+      src={imageUrl}
+      alt="Random Image"
+      className="section-img"
+      style={imageStyle}
+      loading="lazy"
+      title={tooltipText} // Add the title attribute for the tooltip
+    />
+  );
+});
+
+const Section = memo(({ sectionNumber }) => {
+  const [numImages, setNumImages] = useState(
+    Math.floor(Math.random() * 51) + 70
+  );
+  const [sectionHeight, setSectionHeight] = useState(0);
+  const startDate = new Date(new Date().getFullYear(), 9, 7);
+  const currentDate = new Date();
+  const [dates, setDates] = useState([]);
+
+  useEffect(() => {
+    const dateArray = [];
+    let currentDatePointer = new Date(startDate);
+
+    while (currentDatePointer <= currentDate) {
+      dateArray.push(new Date(currentDatePointer));
+      currentDatePointer.setDate(currentDatePointer.getDate() + 1);
+    }
+
+    setDates(dateArray);
+  }, []);
+
+  useEffect(() => {
+    // Calculate the section's height based on the number of images
+    const calculatedHeight = 100 + numImages * 2;
+    setSectionHeight(calculatedHeight);
+  }, [numImages]);
+
+  return (
+    <>
+      {dates.map((date, index) => (
+        <div className="section" style={{ height: sectionHeight }} key={index}>
+          <div className="section-number">
+            <div className="section-numberT">
+              <span className="dash">_</span>
+              {date.toLocaleString("default", { month: "short" })}{" "}
+              {date.getDate()}
+            </div>
+
+            <h1 className="title">
+              <div className="title">{numImages} </div>
+              <span className="drop"></span>
+              <span className="drop"></span>
+              <span className="drop"></span>
+              <span className="drop"></span>
+              <span className="drop"></span>
+            </h1>
+          </div>
+          <div className="section-image">
+            {Array.from({ length: numImages }).map((_, i) => (
+              <Image key={i} sectionHeight={sectionHeight} />
+            ))}
+          </div>
+        </div>
+      ))}
+    </>
+  );
+});
+
+export default Section;
