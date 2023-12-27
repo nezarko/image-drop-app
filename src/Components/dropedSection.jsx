cc92357b84583b8d9faeb434de6ce71926ac6c4e
Nizar.Sawi <nezar056@gmail.com>
2023-12-27 15:27:57 +0200

    dropsection update

--- src/Components/dropedSection.jsx
+++ src/Components/dropedSection.jsx
@@ -1,4 +1,126 @@
-import { memo, useEffect, useRef } from "react";
+// import { memo, useEffect, useRef } from "react";
+// import "../App.css";
+
+// import Image from "./Image";
+
+// import Store from "../Common/Store";
+// import { positionImages } from "../Common/functions";
+
+// const DropedSection = memo(function DropedSection({ sections }) {
+//   /**
+//    * FIXME: ajdust transtion timing at single fall
+//    */
+//   const q = useRef(null);
+
+//   useEffect(() => {
+//     // attache event listiner to observer
+
+//     async function handler(e) {
+//       const {
+//         detail: { target },
+//       } = e;
+//       // dont start excution unless the working function end
+//       q.current
+//         .querySelector(`[data-rc="${target}"]`)
+//         .classList.add("rc-image-append");
+//     }
+
+//     function singel_fall_handler(e) {
+//       const {
+//         detail: {
+//           target: { section, person },
+//         },
+//       } = e;
+//       let a = q.current.querySelector(
+//         `div[data-rc="section-${section}"] div[data-person="person-${person}"]`
+//       );
+
+//       a.classList.add("rc-single-fall");
+//     }
+
+//      window.addEventListener("section:fall", handler);
+//      window.addEventListener("singel:fall", singel_fall_handler);
+
+
+//     return () => {
+//       window.removeEventListener("section:fall", handler);
+//       window.removeEventListener("singel:fall", singel_fall_handler);
+//     };
+//   }, []);
+//   useEffect(() => {console.log("render")})
+
+//   return (
+//     <div
+//       ref={q}
+//       className="section reciver-section-image-container"
+//       id="receiver-section"
+//       style={{
+//         width: "100%",
+//         marginBottom: "10px",
+//         marginTop: "10px",
+//         position: "relative",
+//         boxSizing: "border-box",
+//         padding: "5px",
+//       }}
+//     >
+//       <div
+//         className="zigzag-border"
+//         style={{
+//           position: "absolute",
+//           top: 0,
+//           left: 0,
+//           width: "100%",
+//           height: "0.5%",
+//           zIndex: -1,
+//         }}
+//       ></div>
+
+//       <>
+//         {sections.map((section, index) => {
+//           return (
+//             <div
+//               data-rc={`section-${index}`}
+//               className={`rc-section rc-section-${index} rc-section-img-container reciver-section-image `}
+//               key={index}
+//             >
+//               <div className="section-image">
+//                 {Array.from({ length: section.dataPerson.roses.length }).map(
+//                   (_, i) => (
+//                     <Image
+//                      r={true}
+//                       key={i}
+//                       url={Store.getImage().url}
+//                       top={0}
+//                       left={Math.random() * 90}
+//                       date={section.date}
+//                       index={i}
+//                       iposition={positionImages(i)}
+//                       data-person={`person-${i}`}
+//                       className="section-img section-img-rc"
+//                     />
+//                   )
+//                 )}
+//               </div>
+//             </div>
+//           );
+//         })}
+//       </>
+//     </div>
+//   );
+// })
+
+// export default DropedSection;
+
+
+
+
+
+
+
+
+
+
+import { memo, useEffect, useMemo, useRef } from "react";
 import "../App.css";
 
 import Image from "./Image";
@@ -6,26 +128,34 @@ import Image from "./Image";
 import Store from "../Common/Store";
 import { positionImages } from "../Common/functions";
 
+function debounce(func, delay) {
+  let timeoutId;
+
+  return function (...args) {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      func(...args);
+    }, delay);
+  };
+}
+
+
+
+
 const DropedSection = memo(function DropedSection({ sections }) {
-  /**
-   * FIXME: ajdust transtion timing at single fall
-   */
   const q = useRef(null);
 
-  useEffect(() => {
-    // attache event listiner to observer
-
-    async function handler(e) {
+  const debouncedHandler = useRef(
+    debounce((e) => {
       const {
         detail: { target },
       } = e;
-      // dont start excution unless the working function end
-      q.current
-        .querySelector(`[data-rc="${target}"]`)
-        .classList.add("rc-image-append");
-    }
+      q.current.querySelector(`[data-rc="${target}"]`).classList.add("rc-image-append");
+    }, 300)
+  );
 
-    function singel_fall_handler(e) {
+  const debouncedSingleFallHandler = useRef(
+    debounce((e) => {
       const {
         detail: {
           target: { section, person },
@@ -34,20 +164,38 @@ const DropedSection = memo(function DropedSection({ sections }) {
       let a = q.current.querySelector(
         `div[data-rc="section-${section}"] div[data-person="person-${person}"]`
       );
-
       a.classList.add("rc-single-fall");
-    }
+    }, 300)
+  );
 
-     window.addEventListener("section:fall", handler);
-     window.addEventListener("singel:fall", singel_fall_handler);
-
+  useEffect(() => {
+    window.addEventListener("section:fall", debouncedHandler.current);
+    window.addEventListener("singel:fall", debouncedSingleFallHandler.current);
 
     return () => {
-      window.removeEventListener("section:fall", handler);
-      window.removeEventListener("singel:fall", singel_fall_handler);
+      window.removeEventListener("section:fall", debouncedHandler.current);
+      window.removeEventListener("singel:fall", debouncedSingleFallHandler.current);
     };
   }, []);
-  useEffect(() => {console.log("render")})
+
+  const images = useMemo(() => {
+    return sections.map((section, index) => {
+      return Array.from({ length: section.dataPerson.roses.length }).map((_, i) => (
+        <Image
+          key={`${section.date}-${i}`}
+          r={true}
+          url={Store.getImage().url}
+          top={0}
+          left={Math.random() * 90}
+          date={section.date}
+          index={i}
+          iposition={positionImages(i)}
+          data-person={`person-${i}`}
+          className="section-img section-img-rc"
+        />
+      ));
+    });
+  }, [sections]);
 
   return (
     <div
@@ -63,6 +211,7 @@ const DropedSection = memo(function DropedSection({ sections }) {
         padding: "5px",
       }}
     >
+      {images}
       <div
         className="zigzag-border"
         style={{
@@ -75,38 +224,9 @@ const DropedSection = memo(function DropedSection({ sections }) {
         }}
       ></div>
 
-      <>
-        {sections.map((section, index) => {
-          return (
-            <div
-              data-rc={`section-${index}`}
-              className={`rc-section rc-section-${index} rc-section-img-container reciver-section-image `}
-              key={index}
-            >
-              <div className="section-image">
-                {Array.from({ length: section.dataPerson.roses.length }).map(
-                  (_, i) => (
-                    <Image
-                     r={true}
-                      key={i}
-                      url={Store.getImage().url}
-                      top={0}
-                      left={Math.random() * 90}
-                      date={section.date}
-                      index={i}
-                      iposition={positionImages(i)}
-                      data-person={`person-${i}`}
-                      className="section-img section-img-rc"
-                    />
-                  )
-                )}
-              </div>
-            </div>
-          );
-        })}
-      </>
+     
     </div>
   );
-})
+});
 
 export default DropedSection;
